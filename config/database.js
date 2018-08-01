30267c007a109852fde5643b3097f0daf3f1effb
David Cordova <30476107+daveryde@users.noreply.github.com>
2018-07-31 21:28:22 -0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://<david>:<Stupid123!>@ds249299.mlab.com:49299/vidjot-prod'}
+    module.exports = {mongoURI: 'mongodb://<username>:<password>@ds249299.mlab.com:49299/vidjot-prod'}
 } else {
     module.exports = {mongoURI: 'mongodb://localhost:27017/vidjot-dev'}
 }
