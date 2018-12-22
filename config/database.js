d410cfea9f865932cc726454afa6003c797c461a
David Cordova <d.mcordova87@gmail.com>
2018-12-21 22:22:02 -0700

    atlas db switch

--- config/database.js
+++ config/database.js
@@ -1,8 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = {
-    mongoURI:
-      'mongodb://david:Stupid001!@vidjot-prod-shard-00-00-m9bgo.mongodb.net:27017,vidjot-prod-shard-00-01-m9bgo.mongodb.net:27017,vidjot-prod-shard-00-02-m9bgo.mongodb.net:27017/test?ssl=true&replicaSet=vidjot-prod-shard-0&authSource=admin&retryWrites=true'
-  };
+  module.exports = require('./keys_prod');
 } else {
-  module.exports = { mongoURI: 'mongodb://localhost:27017/vidjot-dev' };
+  module.exports = require('./keys_dev');
 }
