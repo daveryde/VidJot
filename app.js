3f50e759f07e02bfc557bce445b1b277e3c54f3b
David Cordova <d.mcordova87@gmail.com>
2018-12-22 17:54:54 -0700

    configure db keys

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
 const bodyParser = require('body-parser');
@@ -14,7 +14,6 @@ const app = express();
 const ideas = require('./routes/ideas');
 const users = require('./routes/users');
 
-
 // Passport Congif
 require('./config/passport')(passport);
 
@@ -25,15 +24,23 @@ const db = require('./config/database');
 mongoose.Promise = global.Promise;
 
 // Connect to mongoose
-mongoose.connect(db.mongoURI, {
-    useNewUrlParser: true
-})
-.then(() => console.log('MongoDB Connected...'))
-.catch(err => console.log(err));
+mongoose
+  .connect(
+    db.MONGO_URI,
+    {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 // Handlebars Middleware
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'}));
+app.engine(
+  'handlebars',
+  exphbs({
+    defaultLayout: 'main'
+  })
+);
 app.set('view engine', 'handlebars');
 
 // Body Parser Middleware
@@ -47,38 +54,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
 // Express session middleware
-app.use(session({
+app.use(
+  session({
     secret: 'secret',
     resave: true,
     saveUninitialized: true
-  }));
+  })
+);
 
 // Passport Middleware
-  app.use(passport.initialize());
-  app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-  app.use(flash());
+app.use(flash());
 
-  // Global variables
-  app.use(function(req, res, next){
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    res.locals.user = req.user || null;
-    next();
-  });
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
 
 // Index Route
 app.get('/', (req, res) => {
-    const title = 'Welcome';
-    res.render('index', {
-        title: title
-    });
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
+  });
 });
 
 // About Route
 app.get('/about', (req, res) => {
-    res.render('about');
+  res.render('about');
 });
 
 // Use routes
@@ -88,5 +97,5 @@ app.use('/users', users);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
